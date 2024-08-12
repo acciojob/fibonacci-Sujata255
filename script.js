c13bc6c74017babfccf989d7652cacae0c7afade
Sujata255 <das19sujata@gmail.com>
2024-08-12 12:22:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
-// your code here
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+
+    let a = 0, b = 1;
+    for (let i = 2; i <= num; i++) {
+        [a, b] = [b, a + b];
+    }
+    return b;
 }
 
 module.exports = fibonacci;
