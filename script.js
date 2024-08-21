fdb0765e5c7d188916796648ae38f676489ab40b
Sujata255 <das19sujata@gmail.com>
2024-08-21 19:55:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+function fibonacci(num) {
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    }
 
-function fibonacci(N) // returns the Nth Fibonacci Number
-{
-let a = 0; // Pos 1 || 1st Finonacci Number || Position 1 in the Fibo. Series 
-let b = 1;  // Pos 2 || 2nd Finonacci Number || Position 2 in the Fibo. Series 
-let c = 0; // Should be position 3
-let currentPosition = 3;
-while (currentPosition <= N) { // Exit when N+1 happens; so c would've been calculated as Position Nth in the Fibo. Series 
-    c = a + b;
-     a = b; // n-2th
-      b = c; // n-1th value
-    ++currentPosition;
+    let a = 0, b = 1;
+    for (let i = 2; i <= num; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 }
-return c;
-}
+
+// Example usage:
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
+console.log(fibonacci(10)); // Output: 34
