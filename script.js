75cd5830a616646f57a2ec3dfe8c1b7d7bd10778
Sujata255 <das19sujata@gmail.com>
2024-09-13 22:28:52 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,19 @@
+function fibonacci(num)
+
+if(n === 1){
+	return 0;
+}
+	
+else if(n===2){
+	return 1;
+}
+	
+
+let a = 0, b = 1;
+for (let i = 3; i <= num; i++){
+	let temp = a + b;
+	a = b;
+	b = temp;
+}
+return b;
+} module.exports = fibonacci;
